bb8114142d6a0f0434271583c0c5a15e1d5af459
dv1j <dvijpurmanun1@gmail.com>
2026-06-05 09:26:33 +0400

    test 5

--- test/function.c
+++ test/function.c
@@ -1,19 +1,5 @@
 #include "main.h"
 /**
-  *print_char - print char
-  *character - check for character
-  *@args: check for arguments
-  *Return: the char
-  */
-int print_char(va_list args)
-{
-	char character;
-
-	character = va_arg(args, int);
-	_putchar(character);
-	return (1);
-}
-/**
   * print_string - print string
   * @str - check for th string
   * @args: check for the arguments
@@ -36,4 +22,3 @@ int print_string(va_list args)
 	}
 	return (chars_printed);
 }
-
--- test/our_printf.c
+++ test/our_printf.c
@@ -17,9 +17,13 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; format[i]; i++)
 	{
-		if (format[i] == '%' && format[i + 1])
+		if (format[i] == '%')
 		{
 			i++;
+			
+			if(format[i] == '\0')
+				return (-1);
+
 			if (format[i] == 'c')
 			{
 				_putchar(va_arg(args, int));
