b36edd886952772698f05ad17d39ba86e5a7fa74
dv1j <dvijpurmanun1@gmail.com>
2026-06-06 21:02:50 +0400

    digit_helper updated

--- digit_helper.c
+++ digit_helper.c
@@ -1,33 +1,30 @@
 #include "main.h"
 /**
-  * print_number - print the num
-  *@num: number
-  *Return:return count
-  */
-
+ * print_number - print the num
+ *@num: number
+ *Return:return count
+ */
 int print_number(int num)
 {
-int div = 1;
-int count = 0;
-unsigned int n;
-if (num < 0)
-{
-	_putchar('-');
-	count++;
-	n = -num;
-}
-else
-{
-	n = num;
-}
-while (n / div >= 10)
-	div * = 10;
-while (div > 0)
-{
-	_putchar((n / div) + '0');
-	n % = div;
-	div / = 10;
-}
-
-return (count);
+	int div = 1;
+	int digit_printed = 0;
+	unsigned int n;
+	if (num < 0)
+	{
+		digit_printed += _putchar('-');
+		n = -num;
+	}
+	else
+	{
+		n = num;
+	}
+	while (n / div >= 10)
+		div * = 10;
+	while (div > 0)
+	{
+		_putchar((n / div) + '0');
+		n % = div;
+		div / = 10;
+	}
+	return (digit_printed);
 }
